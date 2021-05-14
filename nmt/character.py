7e60244631da4f7c251cf36dbdd6c077b81f8a4a
my1251 <tyu@simulmedia.com>
2021-05-14 16:43:54 -0400

    customize

---
+++ nmt/character.py
@@ -0,0 +1,42 @@
+import csv
+import sys
+import json
+
+csv.field_size_limit(sys.maxsize)
+
+episode =3
+line_text =2
+speaker = 1
+d = {}
+
+name = "../data/scripts.csv"
+train = open("en_es_data/train.en", "w")
+test = open("en_es_data/test.en", "w")
+dev = open("en_es_data/dev.en", "w")
+
+f = open(name)
+
+reader = csv.reader(f)
+next(reader)
+prevRow = next(reader)
+
+vocab = {"<pad>": 0, "<s>": 1, "</s>": 2, "<unk>": 3}
+data = {"src_word2id": speaker_vocab, "tgt_word2id":_vocab}
+count = 1
+
+for currRow in reader:
+    if currRow[speaker] == "JERRY" and prevRow[episode] == currRow[episode]:
+        if int(float(currRow[episode])) <= 15:
+            train.write(currRow[line_text] + "\n")
+        elif int(float(currRow[episode])) <= 19:
+            test.write(currRow[line_text] + "\n")
+        else:
+            dev.write(currRow[line_text] + "\n")
+    for word in currRow[line_text].split(" "):
+        if word not in vocab.keys():
+            vocab[word] = count
+            count += 1
+    prevRow = currRow
+
+write_file = open("vocab.json", "w")
+json.dump(data, write_file)
